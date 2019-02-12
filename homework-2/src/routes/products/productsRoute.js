8cb84104785e2154a689dcc1883bd0950f220bd6
AlxndrYatsenko <oldr.yatsenko@gmail.com>
2019-02-12 17:32:50 +0200

    add get product by category

--- homework-2/src/routes/products/productsRoute.js
+++ homework-2/src/routes/products/productsRoute.js
@@ -25,23 +25,47 @@ const productsRoute = (req, res) => {
       const id = Number(req.url.split("/")[2]);
       // get query from url
       const { query } = url.parse(req.url, true);
-
       if (id) {
         // get product by id
         responseData = parsedData.filter(p => p.id === id) || [];
       } else if (query) {
         // get ids array from url
-        const ids = query.ids
-          .split(",")
-          .map(i => Number(i.replace(/[^-0-9]/gim, "")));
+
+        let queryArr = [];
+
+        if (query.ids) {
+          queryArr = query.ids
+            .split(",")
+            .map(i => Number(i.replace(/[^-0-9]/gim, "")));
+        }
+
+        if (query.category) {
+          queryArr.push(query.category.replace(/[^-a-z]/gim, ""));
+        }
 
         const filtredProducts = () => {
           const arr = [];
-          ids.length > 0
-            ? ids.map(id =>
-                parsedData.map(p => (p.id === id ? arr.push(p) : null))
-              )
-            : [];
+
+          if (query.category) {
+            queryArr.length > 0
+              ? queryArr.map(category =>
+                  parsedData.map(p =>
+                    p.categories.forEach(e =>
+                      e === category ? arr.push(p) : null
+                    )
+                  )
+                )
+              : [];
+          }
+
+          if (query.ids) {
+            queryArr.length > 0
+              ? queryArr.map(id =>
+                  parsedData.map(p => (p.id === id ? arr.push(p) : null))
+                )
+              : [];
+          }
+
           return arr.length > 0
             ? arr.map(({ id, sku, name, description }) => {
                 return { id, sku, name, description };
