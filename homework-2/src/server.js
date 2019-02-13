31ffbabb1a9d729c34a9e30c529f6d946c40486a
AlxndrYatsenko <oldr.yatsenko@gmail.com>
2019-02-13 10:21:16 +0200

    added ssl

--- homework-2/src/server.js
+++ homework-2/src/server.js
@@ -1,4 +1,8 @@
-const http = require("http");
+// const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+// const http = require("http");
+const https = require("https");
 const url = require("url");
 
 const morgan = require("morgan");
@@ -6,8 +10,13 @@ const router = require("./routes/router");
 
 const logger = morgan("combined");
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./ssl/server.key")),
+  cert: fs.readFileSync(path.join(__dirname, "./ssl/server.crt"))
+};
+
 const startServer = port => {
-  const server = http.createServer((req, res) => {
+  const server = https.createServer(options, (req, res) => {
     const parsedUrl = url.parse(req.url);
     const pathName = "/" + parsedUrl.pathname.split("/")[1] || "/";
     const func = router[pathName] || router.default;
