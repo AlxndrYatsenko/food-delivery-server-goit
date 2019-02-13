5c96af3f6de22e52470287703c9b496266ca6f04
AlxndrYatsenko <oldr.yatsenko@gmail.com>
2019-02-13 17:54:34 +0200

    fixed

--- homework-2/src/routes/products/helpers.js
+++ homework-2/src/routes/products/helpers.js
@@ -1,27 +1,26 @@
+const getValues = arr =>
+  arr.map(({ id, sku, name, description }) => {
+    return { id, sku, name, description };
+  });
+
 const getProductsByCategory = (queryArr, parsedData) => {
   const productsArr = [];
-  queryArr
-    .map(category =>
-      parsedData.map(p =>
-        p.categories.forEach(e => {
-          e === category && !productsArr.includes(p) && productsArr.push(p);
-        })
-      )
+  queryArr.map(category =>
+    parsedData.map(p =>
+      p.categories.forEach(e => {
+        e === category && !productsArr.includes(p) && productsArr.push(p);
+      })
     )
-    .map(({ id, sku, name, description }) => {
-      return { id, sku, name, description };
-    });
-  return productsArr;
+  );
+  return getValues(productsArr);
 };
 
 const getProductsById = (queryArr, parsedData) => {
-  return queryArr
-    .map(id => {
-      return parsedData.find(p => id === p.id.toString());
-    })
-    .map(({ id, sku, name, description }) => {
-      return { id, sku, name, description };
-    });
+  const productsArr = [];
+  parsedData.map(p =>
+    queryArr.forEach(id => p.id.toString() === id && productsArr.push(p))
+  );
+  return getValues(productsArr);
 };
 
 module.exports = { getProductsByCategory, getProductsById };
